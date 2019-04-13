1e41c3241715c5ec00506b2e837848e2b050882a
Christian Pinto <pintochristian2@gmail.com>
2019-04-13 20:33:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,9 +8,10 @@ end
 
 
 def not_safe?(speed)
+  speed > 40 && speed < 60 ? false : true
 	return true if speed > 60
 	return true if speed < 40
-	return false if speed > 40 && speed < 60
+	
 end
 	
 
